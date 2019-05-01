897c38006a0409070ead65552b89386936f18eb0
aaron <aaronpedro16@gmail.com>
2019-05-01 16:16:26 -0300

    first commit

---
+++ egressos/db/migrate/20190501174831_create_students.rb
@@ -0,0 +1,14 @@
+class CreateStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.string :nome
+      t.string :matricula
+      t.integer :ano
+      t.integer :semestre
+      t.integer :homologado
+      t.references :course, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
